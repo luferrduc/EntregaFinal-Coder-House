00d64c81c52a730727aabdbf52eacb08d17ec2bc
luferr <luciano.ferrando94@gmail.com>
2023-06-18 15:45:50 -0400

    Arreglo tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/*.{html,js}", "./src/js/*.js" ],
+export default {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-    // colors: {
-    //   'japon-1': '#a6a49b',
-    //   'japon-2': '#e1c6a5',
-    //   'japon-3': '#e7e2d2',
-    //   'japon-4': '#8599a4',
-    //   'japon-5': '#1f2950',
-    //   'japon-6': '#4a5f79',
-    //   'japon-7': '#3d403b',
-    //   'japon-8': '#857d5b',
-    //   'japon-9': '#bba76f',
-    // }
   },
   plugins: [],
 }
